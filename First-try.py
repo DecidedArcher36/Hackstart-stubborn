efaca026aede21156c596476dd7924a7cb956e36
icecubes834 <icecubes834@gmail.com>
2026-09-19 13:02:53 +0100

    Fixed errors

--- First-try.py
+++ First-try.py
@@ -1,2 +1,21 @@
-print("Hi")
-print("Beep Boop")
+
+import tkinter as tk
+from tkinter import *
+
+# opens a window
+root=tk.Tk()
+# sets size of the window
+root.geometry('800x1200')
+
+
+image = PhotoImage(file="Smile.png")
+image_label = tk.Label(root, image=image)
+image_label.pack()
+
+#trying to make text appear when button is pressed
+button = tk.Button(root, text="Click", command=lambda: label.config(text="Hello")).pack(())
+
+label = tk.Label(root)
+label.pack()
+
+root.mainloop()
