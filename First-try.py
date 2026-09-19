3cc04dc4381ac48f957150a38f994bed461bd94e
DecidedArcher36 <ybeh1g26@soton.ac.uk>
2026-09-19 12:44:38 +0100

    Update First-try.py

--- First-try.py
+++ First-try.py
@@ -1,2 +1,29 @@
-print("Hi")
-print("hello")
+import tkinter as tk
+from tkinter import *
+
+print("Hello World")
+
+# opens a window
+root=tk.Tk()
+# sets size of the window
+root.geometry('800x1200')
+
+def makeimage():
+    #trying to show an image on screen
+    image = PhotoImage(file="smile.png")
+    image_label = tk.Label(root, image)
+    image_label.pack()
+
+
+makeimage()
+
+#trying to make text appear when button is pressed
+button = tk.Button(root, text="Click", command=lambda: label.config(text="Hello")).pack(())
+button.pack()
+label = tk.Label(root)
+label.pack()
+
+
+
+
+root.mainloop()
