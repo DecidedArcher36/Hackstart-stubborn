1118cb3e07f199968d0c9c189ad22858ec855cbf
icecubes834 <icecubes834@gmail.com>
2026-09-19 14:01:26 +0100

    Fixed errors

--- First-try.py
+++ First-try.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import tkinter as tk
 from tkinter import *
@@ -6,7 +5,7 @@ from tkinter import *
 # opens a window
 root=tk.Tk()
 # sets size of the window
-root.geometry('800x1200')
+root.geometry('1920x1080')
 
 
 image = PhotoImage(file="Smile.png")
@@ -20,36 +19,3 @@ label = tk.Label(root)
 label.pack()
 
 root.mainloop()
-=======
-
-import tkinter as tk
-from tkinter import *
-
-print("Hello World")
-
-# opens a window
-root=tk.Tk()
-# sets size of the window
-root.geometry('800x1200')
-
-def makeimage():
-    #trying to show an image on screen
-    image = PhotoImage(file="smile.png")
-    image_label = tk.Label(root, image)
-    image_label.pack()
-
-
-makeimage()
-
-#trying to make text appear when button is pressed
-button = tk.Button(root, text="Click", command=lambda: label.config(text="Hello")).pack(())
-button.pack()
-label = tk.Label(root)
-label.pack()
-
-
-
-
-root.mainloop()
-
->>>>>>> c2949351cf49e7a8fd3f4d0bff994f6e72781833
